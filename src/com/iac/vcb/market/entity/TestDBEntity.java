c66dca6cd358a72273e7301b39d42fd36bdf4dc2
mguo <mguo@SI-MGUO1.corp.valueclick.com>
2014-03-03 14:59:25 +0800

    d

--- src/com/iac/vcb/market/entity/TestDBEntity.java
+++ src/com/iac/vcb/market/entity/TestDBEntity.java
@@ -5,13 +5,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import org.hibernate.annotations.GenericGenerator;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name = "miner_test")
-public class TestDBEntity implements java.io.Serializable{
+public class TestDBEntity implements java.io.Serializable {
 
 	/**
 	 * 
@@ -19,13 +22,21 @@ public class TestDBEntity implements java.io.Serializable{
 	private static final long serialVersionUID = 7797631880559599138L;
 
 	@Id
-	@GeneratedValue(generator="increment")
-	@GenericGenerator(name="increment", strategy = "increment") 
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	@Column(name = "id")
+	@JsonProperty(value = "ID")
 	private Integer id;
 
 	@Column(name = "value")
+	@JsonProperty(value = "Value")
 	private String value;
+
+	
+	//非数据库字段，非json输出
+	@Transient
+	@JsonIgnore
+	private String randStr = "this is random string";
 
 	public Integer getId() {
 		return id;
@@ -41,6 +52,18 @@ public class TestDBEntity implements java.io.Serializable{
 
 	public void setValue(String value) {
 		this.value = value;
+	}
+
+	public String getRandStr() {
+		return randStr;
+	}
+
+	public void setRandStr(String randStr) {
+		this.randStr = randStr;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
 
 	public String toString() {
